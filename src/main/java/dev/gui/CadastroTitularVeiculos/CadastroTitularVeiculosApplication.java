b3d2d420b700e6fb42c15eb90970b2f5296eba4d
Guigdsdev <guigdsdev@gmail.com>
2025-11-11 16:54:30 -0300

    Inicio projeto

---
+++ src/main/java/dev/gui/CadastroTitularVeiculos/CadastroTitularVeiculosApplication.java
@@ -0,0 +1,13 @@
+package dev.gui.CadastroTitularVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroTitularVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroTitularVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gui/CadastroTitularVeiculos/CadastroTitularVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gui.CadastroTitularVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroTitularVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
