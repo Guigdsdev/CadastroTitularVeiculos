c27be7fa52e3c6f9862ad4f28636fd804f2b609c
Guigdsdev <guigdsdev@gmail.com>
2025-11-12 17:09:59 -0300

    feat: Cria entidade TitularModel como modelo para tabela no banco de dados

--- src/main/java/dev/gui/CadastroTitularVeiculos/domain/TitularModel.java
+++ src/main/java/dev/gui/CadastroTitularVeiculos/domain/TitularModel.java
@@ -17,10 +17,20 @@ public class TitularModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "idade")
     private int idade;
+
+    @Column(name = "numeracao_carteira")
     private int numeracaoCarteira;
+
+    @Column(name = "endereco")
     private String endereco;
 
     @OneToMany(mappedBy = "titular")
