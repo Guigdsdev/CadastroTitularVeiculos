45c149346d5c8bb45204964554ee6617e193f022
Guigdsdev <guigdsdev@gmail.com>
2025-11-13 22:17:12 -0300

    refactor: Refatora o MapperTitular e finaliza o mapeamento

--- src/main/java/dev/gui/CadastroTitularVeiculos/mappers/MapperTitular.java
+++ src/main/java/dev/gui/CadastroTitularVeiculos/mappers/MapperTitular.java
@@ -2,12 +2,21 @@ package dev.gui.CadastroTitularVeiculos.mappers;
 
 import dev.gui.CadastroTitularVeiculos.DTOs.TitularDTO;
 import dev.gui.CadastroTitularVeiculos.domain.TitularModel;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MapperTitular {
 
     public TitularModel map(TitularDTO titularDTO){
         TitularModel titularModel = new TitularModel();
         titularModel.setId(titularDTO.getId());
+        titularModel.setEmail(titularDTO.getEmail());
+        titularModel.setNome(titularDTO.getNome());
+        titularModel.setIdade(titularDTO.getIdade());
+        titularModel.setNumeracaoCarteira(titularDTO.getNumeracaoCarteira());
+        titularModel.setEndereco(titularDTO.getEndereco());
+        titularModel.setVeiculos(titularDTO.getVeiculos());
+        return titularModel;
     }
 
     public TitularDTO map(TitularModel titularModel){
