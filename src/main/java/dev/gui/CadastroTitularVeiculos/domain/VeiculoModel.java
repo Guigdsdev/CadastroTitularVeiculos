466a7fb933b87aaf52946056489e4649ba6e355a
Guigdsdev <guigdsdev@gmail.com>
2025-11-13 23:50:48 -0300

    refactor: Inclui a annotation @Column no VeiculoModel e nomeia as colunas da tabela no banco de dados

--- src/main/java/dev/gui/CadastroTitularVeiculos/domain/VeiculoModel.java
+++ src/main/java/dev/gui/CadastroTitularVeiculos/domain/VeiculoModel.java
@@ -8,13 +8,25 @@ public class VeiculoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "modelo")
     private String modelo;
+
+    @Column(name = "marca")
     private String marca;
+
+    @Column(name = "ano")
     private int ano;
+
+    @Column(name = "km")
     private double km;
+
+    @Column(name = "velocidade_maxima")
     private int velocidadeMaxima;
+
+    @Column(name = "img_url")
     private String imgUrl;
 
     @ManyToOne
