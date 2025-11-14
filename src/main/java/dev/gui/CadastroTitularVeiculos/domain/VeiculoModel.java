211f65c0990c75ae7f2912c6ed5f83d6c8dce7f1
Guigdsdev <guigdsdev@gmail.com>
2025-11-13 23:52:53 -0300

    refactor: Inclui annotations do lombok na VeiculoModel

--- src/main/java/dev/gui/CadastroTitularVeiculos/domain/VeiculoModel.java
+++ src/main/java/dev/gui/CadastroTitularVeiculos/domain/VeiculoModel.java
@@ -1,7 +1,13 @@
 package dev.gui.CadastroTitularVeiculos.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tb_veiculos")
 public class VeiculoModel {
