d293423cfc174820ea86a733d0ca2905ecdecf26
Guigdsdev <guigdsdev@gmail.com>
2025-12-01 01:15:20 -0300

    refactor: Implementa o inicio dos DTO para o VeiculoService

--- src/main/java/dev/gui/CadastroTitularVeiculos/service/VeiculoService.java
+++ src/main/java/dev/gui/CadastroTitularVeiculos/service/VeiculoService.java
@@ -1,4 +1,32 @@
 package dev.gui.CadastroTitularVeiculos.service;
 
+import dev.gui.CadastroTitularVeiculos.DTOs.VeiculoDTO;
+import dev.gui.CadastroTitularVeiculos.domain.VeiculoModel;
+import dev.gui.CadastroTitularVeiculos.mappers.MapperVeiculo;
+import dev.gui.CadastroTitularVeiculos.repository.VeiculoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class VeiculoService {
+
+    private final VeiculoRepository veiculoRepository;
+    private final MapperVeiculo mapperVeiculo;
+
+    public VeiculoService(VeiculoRepository veiculoRepository, MapperVeiculo mapperVeiculo) {
+        this.veiculoRepository = veiculoRepository;
+        this.mapperVeiculo = mapperVeiculo;
+    }
+
+    public List<VeiculoDTO> listarVeiculos() {
+        List<VeiculoModel> veiculos = veiculoRepository.findAll();
+        return veiculos.stream().map(mapperVeiculo::map).collect(Collectors.toList());
+    }
+
+    public VeiculoDTO criarVeiculo(VeiculoDTO veiculoDTO){
+        VeiculoModel veiculo = new VeiculoModel();
+        veiculo = mapperVeiculo.map(veiculoDTO);
+        veiculoRepository.save(veiculo);
+        return mapperVeiculo.map(veiculo);
+    }
 }
